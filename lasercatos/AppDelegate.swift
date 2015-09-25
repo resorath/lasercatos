884facb927fc4f7619ee326b8865b45ad497e063
Sean Feil <sfeil@c4349b.local>
2015-09-24 19:48:38 -0600

    Initial Commit

---
+++ lasercatos/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  lasercatos
+//
+//  Created by Sean Feil on 2015-09-24.
+//  Copyright (c) 2015 Sean Feil. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
